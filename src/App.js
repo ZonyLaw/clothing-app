0890b285479214fd4d9d51aff7b5d7b7086ac9d1
Zonylaw <sunny_law@hotmail.com>
2022-12-24 16:47:33 +0000

    add scss file to app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-const App=()=> {
+import './categories.styles.scss';
 
+const App=()=> {
   const categories = [
     {
     id: 1,
     title: 'Hats'
     },
     {
-    id: 1,
+    id: 2,
     title: 'Jackets'
     },
     {
-    id: 1,
+    id: 3,
     title: 'Sneakers'
     },
     {
-    id: 1,
+    id: 4,
     title: 'Women'
     },
     {
-    id: 1,
+    id: 5,
     title: 'Men'
     }
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({title})=>(
-        <div className="category-container">
-          {/* <img></img> */}
-          <div className="category-body-container"></div>
+      {categories.map(({title, id})=>(
+        <div className="category-container" key={id}>
+          <div className='background-image' />
+          <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
+          </div>
         </div>
       ))}  
       
