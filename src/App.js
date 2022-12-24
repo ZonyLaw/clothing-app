9b130d61542c5fb52d2d20f861418b98ea9f3a0a
Zonylaw <sunny_law@hotmail.com>
2022-12-24 15:28:34 +0000

    turn card into a component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 const App=()=> {
+
+  const categories = [
+    {
+    id: 1,
+    title: 'Hats'
+    },
+    {
+    id: 1,
+    title: 'Jackets'
+    },
+    {
+    id: 1,
+    title: 'Sneakers'
+    },
+    {
+    id: 1,
+    title: 'Women'
+    },
+    {
+    id: 1,
+    title: 'Men'
+    }
+  ];
+
   return (
-  <div className="categories-container">  
-    <div className="category-container">
-      {/* <img></img> */}
-      <div className="category-body-container"></div>
-        <h2>Hats</h2>
-        <p>Shop Now</p>
+    <div className="categories-container">
+      {categories.map(({title})=>(
+        <div className="category-container">
+          {/* <img></img> */}
+          <div className="category-body-container"></div>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+        </div>
+      ))}  
+      
     </div>
-    <div className="category-container">
-      {/* <img></img> */}
-      <div className="category-body-container"></div>
-        <h2>Jackets</h2>
-        <p>Shop Now</p>
-    </div>
-    <div className="category-container">
-      {/* <img></img> */}
-      <div className="category-body-container"></div>
-        <h2>Sneakers</h2>
-        <p>Shop Now</p>
-    </div>
-    <div className="category-container">
-      {/* <img></img> */}
-      <div className="category-body-container"></div>
-        <h2>Women</h2>
-        <p>Shop Now</p>
-    </div>
-    <div className="category-container">
-      {/* <img></img> */}
-      <div className="category-body-container"></div>
-        <h2>Men</h2>
-        <p>Shop Now</p>
-    </div>
-   </div>
   );
 }
 
