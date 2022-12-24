b2622eeb818757981ab6203c11ce5a22b56ddb7d
Zonylaw <sunny_law@hotmail.com>
2022-12-24 21:16:57 +0000

    refactor code using component files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import './categories.styles.scss';
+import Directory from './components/directory/direcotry.component';
 
 const App=()=> {
   const categories = [
     {
-    id: 1,
-    title: 'Hats'
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-    id: 2,
-    title: 'Jackets'
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-    id: 3,
-    title: 'Sneakers'
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-    id: 4,
-    title: 'Women'
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-    id: 5,
-    title: 'Men'
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
     }
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map(({title, id})=>(
-        <div className="category-container" key={id}>
-          <div className='background-image' />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}  
-      
-    </div>
+    <Directory categories = {categories}/>
   );
 }
 
