51ce8de5fcdbe2b0fac8627fcb34965392234528
Zonylaw <sunny_law@hotmail.com>
2022-12-25 21:57:25 +0000

    create different signing methods.

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,45 @@
-import {signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import {
+    auth,
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth,
+    signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils';
 
 
 const SignIn = () => {
-const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
-    createUserDocumentFromAuth(user);
-}
+
+    useEffect( () => {
+        async function fetchdata(){
+            const response = await getRedirectResult(auth);
+            if (response) {
+                const userDocRef = await createUserDocumentFromAuth(response.user);
+            }
+            console.log(response);
+        }
+        fetchdata();
+    }, []);
+    
+   
+    const logGoogleUser = async () => {
+        const {user} = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+
+    }
+
+// const logGoogleRedirectUser = async () => {
+//     const { user } = await signInWithGoogleRedirect();
+//     console.log({user});
+// }
 
     return(
         <div>
             <h1>Sign In page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sign in with Google Redirect
+            </button>
         </div>
     );
 }
