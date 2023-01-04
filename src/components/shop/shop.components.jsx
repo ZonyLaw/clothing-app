8692d53e739970968a1299290f020fc4519438d8
Zonylaw <sunny_law@hotmail.com>
2023-01-04 00:02:17 +0000

    show 4 cards as preview

--- src/components/shop/shop.components.jsx
+++ src/components/shop/shop.components.jsx
@@ -1,27 +1,22 @@
 import { useContext, Fragment } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../product-card/product-card.componenet";
+import CategoryPreview from "../category-preview/category-preview.component";
 import './shop.styles.scss'
+
 
 const Shop = () => {
     const { categoriesMap } = useContext(CategoriesContext);
     return(
-        <Fragment>
+        <div className="shop-container">
         {
-            Object.keys(categoriesMap).map((title) => (
-                <Fragment key={title}>
-                    <h2>{title}</h2>
-                    <div className='products-container'>
-                        {categoriesMap[title].map((product) => (
-                            <ProductCard key={product.id} product={product}/>
-                        ))}
-                    </div>
+            Object.keys(categoriesMap).map((title) => {
+                const products = categoriesMap[title];
+                return <CategoryPreview key={title} title={title} products={products}/>
+            } 
+            )}
 
-                </Fragment>
-            ))}
-
-        
-        </Fragment>
+            
+        </div>
     )
 }
 
