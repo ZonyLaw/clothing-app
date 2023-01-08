983aa4ed82232b2eed0f0ecbf28556c465b46bb6
Zonylaw <sunny_law@hotmail.com>
2023-01-08 12:41:39 +0000

    update form with styled components and add links to the home page

--- src/components/directory-item/directory-item.components.jsx
+++ src/components/directory-item/directory-item.components.jsx
@@ -1,9 +1,14 @@
 import {DirectoryItemContainer, BackgroundImage, Body} from './directory-item.styles.jsx'
+import { useNavigate } from 'react-router-dom';
 
 const DirectoryItem = ({ category }) =>{
-    const { imageUrl, title } = category;
+    const { imageUrl, title, route } = category;
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route)
+
     return(
-        <DirectoryItemContainer>
+        <DirectoryItemContainer onClick={onNavigateHandler}>
             {/* {console.log({imageUrl})} */}
             <BackgroundImage
             imageUrl={imageUrl}
