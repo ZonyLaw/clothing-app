2cdafab399b7d3058673a29561114775bdcaad42
Zonylaw <sunny_law@hotmail.com>
2023-01-21 14:40:02 +0000

    try to update with redux configstore

--- src/routes/store/categories/category.reducer.js
+++ src/routes/store/categories/category.reducer.js
@@ -1,19 +1,32 @@
-import { CATEGORIES_ACTION_TYPES } from "./category.type";
+// import { CATEGORIES_ACTION_TYPES } from "./category.type";
+import { createSlice } from "@reduxjs/toolkit";
+
 
 export const CATEGORIES_INITAIL_STATE = {
     categoriesMap: {}
 }
 
-export const categoriesReducer = (
-        state = CATEGORIES_INITAIL_STATE, 
-        action = {}
-) =>{
-    const {type, payload } = action;
-
-    switch(type){
-        case CATEGORIES_ACTION_TYPES.SET_CATEGORY_MAP:
-            return {...state, categoriesMap: payload};
-        default:
-            return state;
+export const counterSlice = createSlice({
+    name: "categories",
+    initialState: CATEGORIES_INITAIL_STATE,
+    reducers: {
+        SET_CATEGORY_MAP: (state) => {
+          return{ ...state, categoriesMap: payload}
+        }
     }
-};
+
+})
+export const {SET_CATEGORY_MAP} = counterSlice.actions;
+// export const categoriesReducer = (
+//         state = CATEGORIES_INITAIL_STATE, 
+//         action = {}
+// ) =>{
+//     const {type, payload } = action;
+
+//     switch(type){
+//         case CATEGORIES_ACTION_TYPES.SET_CATEGORY_MAP:
+//             return {...state, categoriesMap: payload};
+//         default:
+//             return state;
+//     }
+// };
--- src/routes/store/store.js
+++ src/routes/store/store.js
@@ -1,14 +1,13 @@
-import {compose, applyMiddleware } from 'redux'
-import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 
+import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from "./root-reducer";
 
 // logger is what is displaying and before it reach the dispatch there is a middle agent it goes through first'
 
-const middlewares = [logger];
-const composeEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = configureStore({ reducer: rootReducer
- 
+
+export const store = configureStore({
+    reducer: {
+        rootReducer: rootReducer,
+    }
 });
